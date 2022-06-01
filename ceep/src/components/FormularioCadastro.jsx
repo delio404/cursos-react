5ba18e38124cb0cb866f8bf7f9c7f436106824ed
delio404 <adelio.a.castro@gmail.com>
2022-06-01 17:45:04 -0300

    integrando js

--- ceep/src/components/FormularioCadastro.jsx
+++ ceep/src/components/FormularioCadastro.jsx
@@ -1,16 +1,16 @@
+
 import React, { Component } from "react";
 
 class FormularioCadastro extends Component {
-    render() {
-        return (
-            <form>
-                <input type='text' placeholder='Titulo'></input>
-                <textarea placeholder='Escreva sua nota'></textarea>
-                <button>criar nota</button>
-            </form>
-        )
-    }
-
+  render() {
+    return (
+      <form>
+        <input type="text" placeholder="Título" />
+        <textarea placeholder="Escreva sua nota..." />
+        <button>Criar Nota</button>
+      </form>
+    );
+  }
 }
 
-export default FormularioCadastro
+export default FormularioCadastro;
--- ceep/src/components/ListaDeNotas.jsx
+++ ceep/src/components/ListaDeNotas.jsx
@@ -1,25 +1,21 @@
 import React, { Component } from "react";
 import CardNota from "./CardNota";
 
-
-
 class ListaDeNotas extends Component {
-    render() {
-        return (
-            <ul>
-                <li>
-                   <CardNota/>
-                </li>
-                <li>
-                    <CardNota/>
-                </li>
-                <li>
-                    <CardNota/>
-                </li>
-            </ul>
-        )
-    }
-
-
+  render() {
+    return (
+      <ul>
+        {Array.of("Trabalho", "Trabalho", "Estudos").map((categoria) => {
+          return (
+            <li>
+              
+              <CardNota />
+            </li>
+          );
+        })}
+      </ul>
+    );
+  }
 }
-export default ListaDeNotas
+
+export default ListaDeNotas;
